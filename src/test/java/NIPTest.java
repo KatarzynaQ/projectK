d945af0a39d0bb2bf913716d3e8650feae64bed9
Katarzyna <kkubikka@gmail.com>
2018-08-27 20:00:45 +0200

    test

--- src/test/java/NIPTest.java
+++ src/test/java/NIPTest.java
@@ -1,9 +1,20 @@
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 public class NIPTest {
-    @Test
-    public void NIPTest(){
+
+    private String toCheckNumber;
+    private String correctNip;
+
+    @BeforeEach
+    public void initialize(){
         
+    }
+
+
+    @Test
+    public void NIPOnlyIntegersFromStringTest(){
+
     }
 
 }
