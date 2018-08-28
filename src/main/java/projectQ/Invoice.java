cd829ce759987bc3a923e392d10e5602ddbfd0d1
Katarzyna <kkubikka@gmail.com>
2018-08-28 18:03:08 +0200

    invoice beginning

--- src/main/java/projectQ/Invoice.java
+++ src/main/java/projectQ/Invoice.java
@@ -1,4 +1,11 @@
 package projectQ;
 
+import java.util.Date;
+
 public class Invoice {
+    private Long ID;
+    private Company seller;
+    private Company buyer;
+    private Date invoiceDate;
+    private Date 
 }
--- src/test/java/NIPTest.java
+++ src/test/java/NIPTest.java
@@ -1,20 +1,40 @@
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
+import projectQ.NIP;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NIPTest {
 
     private String toCheckNumber;
     private String correctNip;
+    private NIP nipTesting;
 
-    @BeforeEach
-    public void initialize(){
-        
+    @Before
+    public void initialize() {
+        toCheckNumber = "953-253kj56-63";
+        correctNip = "953-253-56-63";
+        nipTesting = new NIP();
     }
 
 
     @Test
-    public void NIPOnlyIntegersFromStringTest(){
+    public void NIPOnlyIntegersFromStringTest() {
+        List<Integer> result = new ArrayList<Integer>();
+        result.add(9);
+        result.add(5);
+        result.add(3);
+        result.add(2);
+        result.add(5);
+        result.add(3);
+        result.add(5);
+        result.add(6);
+        result.add(6);
+        result.add(3);
+        System.out.println(result);
 
+        assertEquals(result, NIP.onlyIntegersFromString(toCheckNumber));
     }
 
 }
