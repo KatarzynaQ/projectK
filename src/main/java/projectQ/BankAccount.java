d79ce4b91e7a080b06148b468dbe2c9b94ac4cb5
Kamil Grabowski <kamil.grabowski89x@gmail.com>
2018-08-28 18:16:42 +0200

    add account clas

--- src/main/java/projectQ/BankAccount.java
+++ src/main/java/projectQ/BankAccount.java
@@ -1,4 +1,17 @@
 package projectQ;
 
+import lombok.Data;
+
+import java.util.Date;
+
 public class BankAccount {
+
+    private String titleNumber;
+    private String NIP;
+    private Double amount;
+    private Data data;
+    private Enum operationType;
+
+
+
 }
