2efb0d1760a2e48d139898b457c16d08650710c6
Kamil Grabowski <kamil.grabowski89x@gmail.com>
2018-08-29 18:48:02 +0200

    fix

--- src/test/java/projectQ/BankAccountParserTest.java
+++ src/test/java/projectQ/BankAccountParserTest.java
@@ -3,6 +3,7 @@ package projectQ;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,19 +11,19 @@ import java.util.List;
 public class BankAccountParserTest {
 
     @Test
-    public void readBankPaymentTest() throws ParseException {
+    public void readBankPaymentTest() throws ParseException, IOException {
 
         BankAccount i = new BankAccount();
 
         i.setData(i.parsingDateFromString("01-09-2018"));
         i.setAmount(3.69);
-        i.setNIP("5621807397");
+        i.setNIP(new NIP("5621807397"));
         i.setOperationType(OperationType.IN);
         i.setTitleNumber("FS/1/2018");
 
 
 
-        String path = "D:\\workspace\\projectK\\account.csv";
+        String path = "c\\workspace\\account.csv";
         BankAccountParser j = new BankAccountParser();
 
         try {
--- src/test/java/projectQ/CompanyParserTest.java
+++ src/test/java/projectQ/CompanyParserTest.java
@@ -8,12 +8,12 @@ import static org.junit.Assert.assertEquals;
 
 public class CompanyParserTest {
 
-//    @Test
-//    public void CompanyParserTest() throws IOException {
-//        CompanyParser cp=new CompanyParser();
-//        Company company = new Company();
-//        company.setNip(new NIP("9532535663"));
-//        company.setName("ABC1");
-//        assertEquals(company, cp.readCompanies());
-//    }
+    @Test
+    public void CompanyParserTest() throws IOException {
+        CompanyParser cp = new CompanyParser();
+        Company company = new Company();
+        company.setNip(new NIP("9532535663"));
+        company.setName("ABC1");
+        assertEquals(company, cp.readCompanies());
+    }
 }
