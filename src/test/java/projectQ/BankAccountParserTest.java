6e14c1932c11cb0dd17c3026797d21104d917538
Kamil Grabowski <kamil.grabowski89x@gmail.com>
2018-08-29 18:13:06 +0200

    addBankAccountTestwer2

--- src/test/java/projectQ/BankAccountParserTest.java
+++ src/test/java/projectQ/BankAccountParserTest.java
@@ -1,5 +1,6 @@
 package projectQ;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
@@ -9,25 +10,30 @@ import java.util.List;
 public class BankAccountParserTest {
 
     @Test
-    public void readBankPaymentTest() {
+    public void readBankPaymentTest() throws ParseException {
 
         BankAccount i = new BankAccount();
 
-        i.setData(i);
-        i.setAmount();
+        i.setData(i.parsingDateFromString("01-09-2018"));
+        i.setAmount(3.69);
+        i.setNIP("5621807397");
+        i.setOperationType(OperationType.IN);
+        i.setTitleNumber("FS/1/2018");
 
 
 
         String path = "C:\\workspace\\account.csv";
-        BankAccountParser i = new BankAccountParser();
+        BankAccountParser j = new BankAccountParser();
 
         try {
             List <BankAccount> bList = new ArrayList <>();
-            bList = i.readBankPayment(path);
+            bList = j.readBankPayment(path);
             System.out.println(bList);
         } catch (
                 ParseException e) {
             e.printStackTrace();
         }
+
+        Assert.assertEquals(i,j);
     }
 }
