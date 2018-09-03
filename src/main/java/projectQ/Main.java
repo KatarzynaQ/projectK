8f76e7c2c43222adba4c77bd2c0b8f3d022e599e
Kamil Grabowski <kamil.grabowski89x@gmail.com>
2018-09-03 19:17:11 +0200

    test

--- src/main/java/projectQ/Main.java
+++ src/main/java/projectQ/Main.java
@@ -3,7 +3,7 @@ package projectQ;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello");
+
 UserInterface ui=new UserInterface();
 Model model=ui.modelBuilder();
 ui.choseMethod();
